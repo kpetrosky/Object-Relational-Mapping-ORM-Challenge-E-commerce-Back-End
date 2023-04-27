b84f028ae3187fb569d31f52d079a4a0f2057057
kpetrosky <kpetrosky36@gmail.com>
2023-04-26 21:22:07 -0700

    added some installs

--- Develop/server.js
+++ Develop/server.js
@@ -1,3 +1,13 @@
+//npm i inquirer@8.2.4
+//npm i dotenv mysql2 console.table
+//npm init -y
+//mysql -u root -p
+//SOURCE schema.sql;
+//SOURCE insert-schema.sql;
+//SHOW databases;
+//SHOW tables;
+//node server.js
+
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
