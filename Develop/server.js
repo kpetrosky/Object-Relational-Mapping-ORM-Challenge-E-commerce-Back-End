ba30386e4703577df6c33ba76127c18193263137
kpetrosky <kpetrosky36@gmail.com>
2023-04-29 09:49:35 -0700

    got the schema, and seeds to attached correctly, also was able to get server.js to listening. Local host is not working correctly at this time.

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,8 @@
 //npm i inquirer@8.2.4
+//npm i express
 //npm install --save-dev sequelize-cli
 //npm i dotenv mysql2 console.table
+//npm install --save sequelize
 //npm init -y
 //mysql -u root -p
 //SOURCE schema.sql;
@@ -10,7 +12,15 @@
 //node server.js
 
 const express = require('express');
-const sequelize = require('./config/connection');
+const routes = require('./routes');
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize('ecommerce_db', 'root', process.env.DB_PASSWORD, {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -18,7 +28,17 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-sequelize.sync().then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+// turn on routes
+app.use(routes);
 
+// turn on connection to db and server
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connected to the ecommerce_db database.');
+    app.listen(PORT, () => console.log('Now listening'));
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
+  
+module.exports = sequelize;
